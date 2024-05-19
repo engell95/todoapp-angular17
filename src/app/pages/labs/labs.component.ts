ef27099d289d343ddaeff87cc46bbe6470d478cd
Engell Lopez <engelllopez59@gmail.com>
2024-05-19 11:51:43 -0600

    pruebas de laboratorio

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,16 +1,43 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css'
 })
 export class LabsComponent {
   title = 'todoapp';
-  task =[
+  tasks =[
     'Instalar el angular CLI',
-    'dd'
-  ]
+    'Crear proyecto',
+    'Crear Componente',
+    'Crear Servicio'
+  ];
+  name= signal('Engell');
+  age=28;
+  disabled = true;
+  img='https:/w3schools.com/hwoto/img_avatar.png';
+
+  person={
+    name:'Engell',
+    age:28,
+    avatar:'https:/w3schools.com/hwoto/img_avatar.png'
+  };
+  
+  clickHandler(){
+    alert('hola');
+  }
+
+  changeHandler(event: Event){
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.name.set(newValue);
+
+    console.log(event);
+  }
+  
+
 }
