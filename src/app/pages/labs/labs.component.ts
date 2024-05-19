e4b49fc09e96ad845097b040bdccd7fd7eb3d731
Engell Lopez <engelllopez59@gmail.com>
2024-05-19 09:48:57 -0600

    todoapp_angular

---
+++ src/app/pages/labs/labs.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-labs',
+  standalone: true,
+  imports: [],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.css'
+})
+export class LabsComponent {
+  title = 'todoapp';
+  task =[
+    'Instalar el angular CLI',
+    'dd'
+  ]
+}
